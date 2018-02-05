184b07a56fdd98526ec2421989b94fd77b837d87
Jan-Patrick Bollström <karahobny@gmail.com>
2018-02-05 15:19:59 +0200

    plan9 manpages manpath added to work with acme

--- bin/p9p/acme-start.sh
+++ bin/p9p/acme-start.sh
@@ -9,19 +9,22 @@ EDITOR=E
 
 PLAN9=/usr/local/plan9
 PATH=$PATH:$PLAN9/bin
+MANPATH=$MANPATH:$PLAN9/man
 SHELL=$PLAN9/bin/rc
 
 export BROWSER TERM MANPAGER \
-	PAGER EDITOR PLAN9 PATH SHELL
+PAGER EDITOR PLAN9 PATH MANPATH SHELL
 unset FCEDIT VISUAL
 
+fontserv &
+#export font=/mnt/font/Cousine/8a/font
 tabstop=4
 acmeshell=$PLAN9/bin/rc
 
 export tabstop acmeshell
 
-plumber
+plumber &
 cat $HOME/lib/plumbing | 9p write plumb/rules
 cat $PLAN9/plumb/basic | 9p write plumb/rules
 
-exec acme -f /usr/local/plan9/font/mntcarlo/mntcarlo.font $*
+exec acme $*
